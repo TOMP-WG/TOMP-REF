fbe7a3b3036ec7934281f95facdc6553ac7b3605
Geert Tasseron <GTasseron@dat.nl>
2020-05-04 12:34:52 +0200

    Add leaflet map component and click handler

--- tomp-api-web-app/src/app/components/plan/map/map.component.ts
+++ tomp-api-web-app/src/app/components/plan/map/map.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { latLng, Map, tileLayer, LeafletMouseEvent } from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements AfterViewInit {
+
+  private map: Map;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = new Map('map', {
+      center: latLng(51.528833, 3.759161),
+      zoom: 6
+    });
+
+    const tiles = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+
+    this.map.on('click', this.onMapClick);
+  }
+
+  private onMapClick(e: LeafletMouseEvent) {
+    console.log('You clicked the map at ' + e.latlng.toString());
   }
 
 }
